14e97138c40a8405234fc1c91e974b60c50b6b43
Carl <jcaamrelo@gmail.com>
2024-11-10 20:52:46 +0800

    Something

---
+++ CIRCUIT/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CIRCUIT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CIRCUIT/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CIRCUIT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int num = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Something();
+        }
+
+        private void Something()
+        {
+            num = 20;
+        }
+    }
+}
