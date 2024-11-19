b3652e9b84a9996e28ccee7639e7a40dcd7b4932
STRhenz <rhenzganotice28@gmail.com>
2024-11-19 08:05:49 +0800

    connections

--- CIRCUIT/ViewModel/CashierViewModel/ProductDetailsViewModel.cs
+++ CIRCUIT/ViewModel/CashierViewModel/ProductDetailsViewModel.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CIRCUIT.ViewModel.CashierViewModel
-{
-    class ProductDetailsViewModel
-    {
-    }
-}
+﻿//wala TO IDELETE KO NA TYO EH HAHAHAAHAHHA
