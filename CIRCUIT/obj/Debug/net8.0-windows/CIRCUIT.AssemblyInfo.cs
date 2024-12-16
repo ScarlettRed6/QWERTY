27fe585f963f0afab1b671c4a6b159d679d50d16
Carl!! <jcaamrelo@gmail.com>
2024-12-16 19:47:06 +0800

    some commit

--- CIRCUIT/obj/Debug/net8.0-windows/CIRCUIT.AssemblyInfo.cs
+++ CIRCUIT/obj/Debug/net8.0-windows/CIRCUIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIRCUIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e021cd6b7173dcedd1fd5777d4b7e67f5e1a417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccfcd6a079700073e03bef6287c091db29a96b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIRCUIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIRCUIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
